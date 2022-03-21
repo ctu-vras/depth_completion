919877d47eacff54a4f030e8ee87b91590a800d8
RuslanAgishev <agishrus@fel.cvut.cz>
2022-03-21 17:15:40 +0100

    intrinsics and depth map loading

--- src/supervised_depth_correction/data.py
+++ src/supervised_depth_correction/data.py
@@ -9,10 +9,12 @@ from scipy.spatial.transform import Rotation
 from scipy.spatial.transform import Slerp
 from tqdm import tqdm
 import datetime
+from PIL import Image
 
 
 # TODO: use os and file path instead
-data_dir = "/home/ruslan/data/datasets/kitti_raw"
+data_dir_raw = "/home/ruslan/data/datasets/kitti_raw"
+data_dir_depth = "/home/ruslan/data/datasets/kitti_depth"
 
 sequence_names = [
     '2011_09_26',
@@ -27,10 +29,12 @@ class KITTIRawPoses(object):
 
     def __init__(self, seq, subseq, path=None):
         if path is None:
-            path = os.path.join(data_dir, seq, subseq)
+            path = os.path.join(data_dir_raw, seq)
         self.path = path
+        self.subseq = subseq
         self.poses = self.get_poses()
         self.ts = self.get_timestamps(sensor='gps')
+        self.calibrations = self.load_calib()
         self.ids = range(len(self.ts))
 
     @staticmethod
@@ -70,7 +74,7 @@ class KITTIRawPoses(object):
 
     def get_poses(self, zero_origin=False):
         poses = []
-        for fname in np.sort(glob.glob(os.path.join(self.path, 'oxts/data/*.txt'))):
+        for fname in np.sort(glob.glob(os.path.join(self.path, self.subseq, 'oxts/data/*.txt'))):
             pose = self.get_gps_pose(fname)
             poses.append(pose)
         poses = np.asarray(poses)
@@ -90,7 +94,7 @@ class KITTIRawPoses(object):
         else:
             raise ValueError
         timestamps = []
-        ts = np.genfromtxt(os.path.join(self.path, folder, 'timestamps.txt'), dtype=str)
+        ts = np.genfromtxt(os.path.join(self.path, self.subseq, folder, 'timestamps.txt'), dtype=str)
         for t in ts:
             date = t[0]
             day_time, sec = t[1].split(".")
@@ -100,6 +104,42 @@ class KITTIRawPoses(object):
         if zero_origin:
             timestamps = [t - timestamps[0] for t in timestamps]
         return timestamps
+
+    def load_calib(self):
+        """
+        load calib poses and times.
+        """
+        calib = self.parse_calibration(os.path.join(self.path, "calib_cam_to_cam.txt"))
+        return calib
+
+    @staticmethod
+    def parse_calibration(filename):
+        """ read calibration file with given filename
+
+            Returns
+            -------
+            dict
+                Calibration matrices as 4x4 numpy arrays.
+        """
+        calib = {}
+        calib_file = open(filename)
+        for line in calib_file:
+            # load intrinsics
+            if 'K_' in line:
+                key, content = line.split(':')
+                values = [float(v) for v in content.strip().split()]
+
+                K = np.zeros((3, 3))
+                K[0, 0:3] = values[0:3]
+                K[1, 0:3] = values[3:6]
+                K[2, 0:3] = values[6:9]
+
+                calib[key] = K
+        calib_file.close()
+        return calib
+
+    def get_intrins(self):
+        pass
 
     def __len__(self):
         return len(self.ids)
@@ -114,12 +154,46 @@ class KITTIRawPoses(object):
 
 class KITTIDepth:
     # TODO: add rgb-d data from KITTI depth: http://www.cvlibs.net/datasets/kitti/eval_depth.php?benchmark=depth_completion
-    def __init__(self):
+    def __init__(self, seq, mode='val', path=None):
+        assert mode == 'train' or mode == 'val'
+        if path is None:
+            path = os.path.join(data_dir_depth, mode, seq)
+        self.path = path
         self.ts = None
         self.depths = None
+        self.name = seq
 
-    def get_rgbd(self):
-        pass
+    @staticmethod
+    def read_depth(filename):
+        """
+        Depth maps (annotated and raw Velodyne scans) are saved as uint16 PNG images,
+        which can be opened with either MATLAB, libpng++ or the latest version of
+        Python's pillow (from PIL import Image). A 0 value indicates an invalid pixel
+        (ie, no ground truth exists, or the estimation algorithm didn't produce an
+        estimate for that pixel). Otherwise, the depth for a pixel can be computed
+        in meters by converting the uint16 value to float and dividing it by 256.0:
+
+        disp(u,v)  = ((float)I(u,v))/256.0;
+        valid(u,v) = I(u,v)>0;
+        Args:
+            filename: str, full path to depth image
+
+        Returns:
+            depth: np.array, depth image
+        """
+        depth_png = np.array(Image.open(filename), dtype=int)
+        # make sure we have a proper 16bit depth map here.. not 8bit!
+        assert (np.max(depth_png) > 255)
+        depth = depth_png.astype(np.float) / 256.
+        depth[depth_png == 0] = -1.
+        return depth
+
+    def get_depth_map(self, id, gt=False, cam='left'):
+        depth_folder = 'groundtruth' if gt else 'velodyne_raw'
+        camera_n = 2 if cam == 'left' else 3
+        fpath = os.path.join(self.path, 'proj_depth', depth_folder, 'image_%02d/%010d.png' % (camera_n, id))
+        depth_map = self.read_depth(fpath)
+        return depth_map
 
     def get_timestamps(self):
         pass
@@ -151,7 +225,7 @@ def poses_demo():
     # np.random.seed(135)
     seq = np.random.choice(sequence_names)
     while True:
-        subseq = np.random.choice(os.listdir(os.path.join(data_dir, seq)))
+        subseq = np.random.choice(os.listdir(os.path.join(data_dir_raw, seq)))
         if '2011_' in subseq:
             break
     ds = KITTIRawPoses(seq=seq, subseq=subseq)
@@ -176,11 +250,11 @@ def poses_demo():
     plt.show()
 
 
-def ts_demo():
+def gps_velo_times_demo():
     # np.random.seed(135)
     seq = np.random.choice(sequence_names)
     while True:
-        subseq = np.random.choice(os.listdir(os.path.join(data_dir, seq)))
+        subseq = np.random.choice(os.listdir(os.path.join(data_dir_raw, seq)))
         if '2011_' in subseq:
             break
     ds = KITTIRawPoses(seq=seq, subseq=subseq)
@@ -196,10 +270,17 @@ def ts_demo():
     plt.show()
 
 
+def depths_demo():
+    seq = '2011_09_26_drive_0002_sync'
+    ds = KITTIDepth(seq=seq)
+    ds.get_depth_map(5)
+
+
 def main():
     for _ in range(3):
         poses_demo()
-    ts_demo()
+    gps_velo_times_demo()
+    depths_demo()
 
 
 if __name__ == '__main__':
