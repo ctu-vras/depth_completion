e49a996a67292db36abbdc47dd8f46621dee1e98
RuslanAgishev <agishrus@fel.cvut.cz>
2022-05-03 16:20:00 +0200

    separate translation and rotation error option

--- src/supervised_depth_correction/metrics.py
+++ src/supervised_depth_correction/metrics.py
@@ -36,21 +36,29 @@ def MAE(gt, pred, mask=True):
     return mae
 
 
-def localization_accuracy(poses1, poses2):
+def localization_accuracy(poses1, poses2, trans_rot_combined=True):
     """
     Computes sum of euclidian distances between carthesian coordinates of each transformation matrix
     Args:
         poses1: list of <torch.tensor>
         poses2: list of <torch.tensor>
+        trans_rot_combined: bool, Whether to return sum of translational and rotational errors or separate values
 
     Returns: <torch.tensor>
     """
     assert len(poses1) == len(poses2)
-    err = 0
+    err_dist = 0
+    err_rot = 0
     for i in range(len(poses1)):
         pose1 = poses1[i]
         pose2 = poses2[i]
         delta_T = delta_transform(pose1.squeeze(), pose2.squeeze())
-        dist, rot_dist = translation_norm(delta_T), rotation_angle(delta_T)
-        err += dist + rot_dist
-    return err/len(poses1)
+        trans_delta, rot_delta = translation_norm(delta_T), rotation_angle(delta_T)
+        err_dist += trans_delta
+        err_rot += rot_delta
+    err_dist /= len(poses1)
+    err_rot /= len(poses1)
+    if trans_rot_combined:
+        return err_dist + err_rot
+    else:
+        return err_dist, err_rot
